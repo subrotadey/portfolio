e7811caabd34892f8712d1193c3507725d80c970
Subrota  Dey <78078777+subrotadey@users.noreply.github.com>
2021-12-02 02:50:03 +0600

    navbar & header added

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Subrota Dey',
+    title: "Web Developer || Web Designer",
+    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    image: 'https://subrotadey.github.io/car-pictures/img.png',
+    resumePdf: resume
+}
