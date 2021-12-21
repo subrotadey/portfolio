90fb403091eda2865aec2358852a51619e3eeb21
Subrota  Dey <78078777+subrotadey@users.noreply.github.com>
2021-12-21 22:22:05 +0600

    contact form updated

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { Snackbar, IconButton, SnackbarContent } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import axios from 'axios';
-import isEmail from 'validator/lib/isEmail';
+// import axios from 'axios';
+// import isEmail from 'validator/lib/isEmail';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     FaTwitter,
@@ -34,8 +34,8 @@ function Contacts() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
-    const [success, setSuccess] = useState(false);
-    const [errMsg, setErrMsg] = useState('');
+    const [success] = useState(false);
+    const [errMsg] = useState('');
 
     const { theme } = useContext(ThemeContext);
 
@@ -129,36 +129,36 @@ function Contacts() {
 
     const classes = useStyles();
 
-    const handleContactForm = (e) => {
-        e.preventDefault();
+    // const handleContactForm = (e) => {
+    //     e.preventDefault();
 
-        if (name && email && message) {
-            if (isEmail(email)) {
-                const responseData = {
-                    name: name,
-                    email: email,
-                    message: message,
-                };
+    //     if (name && email && message) {
+    //         if (isEmail(email)) {
+    //             const responseData = {
+    //                 name: name,
+    //                 email: email,
+    //                 message: message,
+    //             };
 
-                axios.post(contactsData.sheetAPI, responseData).then((res) => {
-                    console.log('success');
-                    setSuccess(true);
-                    setErrMsg('');
+    //             axios.post(contactsData.sheetAPI, responseData).then((res) => {
+    //                 console.log('success');
+    //                 setSuccess(true);
+    //                 setErrMsg('');
 
-                    setName('');
-                    setEmail('');
-                    setMessage('');
-                    setOpen(false);
-                });
-            } else {
-                setErrMsg('Invalid email');
-                setOpen(true);
-            }
-        } else {
-            setErrMsg('Enter all the fields');
-            setOpen(true);
-        }
-    };
+    //                 setName('');
+    //                 setEmail('');
+    //                 setMessage('');
+    //                 setOpen(false);
+    //             });
+    //         } else {
+    //             setErrMsg('Invalid email');
+    //             setOpen(true);
+    //         }
+    //     } else {
+    //         setErrMsg('Enter all the fields');
+    //         setOpen(true);
+    //     }
+    // };
 
     return (
         <div
